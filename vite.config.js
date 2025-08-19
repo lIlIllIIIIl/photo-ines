e699fc33ca42d15e3c3e8d75b6ae659c7f368d2a
lIlIllIIIIl <alexis.kessab@orange.fr>
2025-08-19 18:31:12 +0200

    feat: add autoImport

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
+import { URL, fileURLToPath } from 'node:url'
+
+import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/,
+        /\.vue$/,
+        /\.vue\?vue/,
+        /\.md$/,
+      ],
+      imports: ['vue', '@vueuse/core'],
+      dirs: [
+        './composables/**',
+      ],
+      eslintrc: { enabled: true },
+      dts: true
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
